db1a97e8f08fe0da97d767839035dd495b75e5be
SandeSydney <sandesydney01@gmail.com>
2020-10-20 19:33:35 +0300

    Used Gesture Detector widget, added ServicesOffered facility

--- lib/widgets/home_list_view.dart
+++ lib/widgets/home_list_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:habs/pages/about.dart';
 import 'package:habs/pages/appointment.dart';
 import 'package:habs/pages/doctors.dart';
+import 'package:habs/pages/hospital_services.dart';
 
 /// Class responsible for building the display of the Homepage of the application
 class HomeListView extends StatefulWidget {
@@ -13,14 +14,14 @@ class _HomeListViewState extends State<HomeListView> {
   // method to build items for the ListView
   Widget buildItem({String image}) {
     // making the images on the home scrollview clickable
-    return FlatButton(
-      padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 0.0),
-      // using an image as the child of the FlatButton
+    return GestureDetector(
+//      padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 0.0),
+      //using an image as the child of the GestureDetector
       child: Image.asset(
         'assets/images/$image.jpg',
         width: double.infinity,
       ),
-      onPressed: () {
+      onTap: () {
         // determine the image that has been clicked and open the necessary class handle it
         switch (image) {
           case 'logo':
@@ -37,11 +38,18 @@ class _HomeListViewState extends State<HomeListView> {
                   builder: (context) => Doctors(),
                 ));
             break;
-          case 'bg':
+          case 'Appointment':
             Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => Appointments(),
+                ));
+            break;
+          case 'ServicesOffered':
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HospitalServices(),
                 ));
             break;
         }
@@ -55,8 +63,12 @@ class _HomeListViewState extends State<HomeListView> {
     return ListView(
       children: <Widget>[
         buildItem(image: 'logo'),
+        SizedBox(height: 5),
         buildItem(image: 'Doctors'),
-        buildItem(image: 'bg'),
+        SizedBox(height: 5),
+        buildItem(image: 'Appointment'),
+        SizedBox(height: 5),
+        buildItem(image: 'ServicesOffered')
       ],
     );
   }

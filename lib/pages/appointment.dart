25d81c0f1725b8a149950e0cd31588cfdc7564c4
SandeSydney <sandesydney01@gmail.com>
2020-10-23 15:27:14 +0300

    Added the Appointment form

--- lib/pages/appointment.dart
+++ lib/pages/appointment.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:date_time_picker/date_time_picker.dart';
 
-// this class lists the services that are offered at the hospital
-class Appointments extends StatelessWidget {
+class Appointments extends StatefulWidget {
+  @override
+  _AppointmentsState createState() => _AppointmentsState();
+}
+
+class _AppointmentsState extends State<Appointments> {
+  // create the key of the form, this allows the validation of the form
+  final _formKey = GlobalKey<FormState>();
+
+  // variables to hold data to be collected from the form
+  String _userName, _email, _phoneNumber;
+  DateTime _appDate, _appStartTime, _appLastTime;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,10 +21,116 @@ class Appointments extends StatelessWidget {
       appBar: AppBar(
         title: Text('Book Appointment'),
       ),
-      body: Center(
-        child: Icon(
-          Icons.assignment,
-          size: 120.0,
+      // building a form using the form key stated above
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                textCapitalization: TextCapitalization.words,
+                autocorrect: false,
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Please enter Last Name!';
+                  }
+                },
+                onSaved: (input) => _userName = input,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey.shade200,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey.shade200,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  ),
+                  prefixIcon: Icon(Icons.person),
+                  labelText: 'Full Name',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Please enter email!';
+                  }
+                },
+                onSaved: (input) => _email = input,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey.shade200,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey.shade200,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  ),
+                  prefixIcon: Icon(Icons.email),
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.phone,
+                validator: (input) {
+                  if (input.length > 10 &&
+                      input.length <= 0 &&
+                      input.length != 10) {
+                    return 'Please enter a valid Phone Number!';
+                  }
+                },
+                onSaved: (input) => _phoneNumber = input,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey.shade200,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30.0),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueGrey.shade200,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30.0),
+                    ),
+                  ),
+                  prefixIcon: Icon(Icons.phone_android),
+                  labelText: 'Phone Number',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              DateTimePicker(
+                type: DateTimePickerType.dateTimeSeparate,
+                dateMask: 'd MMM, yyyy',
+                initialValue: DateTime.now().toString(),
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100),
+                dateLabelText: 'Date',
+                timeLabelText: 'Hour',
+                icon: Icon(Icons.event),
+              ),
+            ],
+          ),
         ),
       ),
     );

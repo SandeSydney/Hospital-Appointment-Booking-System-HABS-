22715d716c0e555e7260cbcabe292c51a0257fc7
SandeSydney <sandesydney01@gmail.com>
2020-10-26 05:10:52 +0300

    Included the Log Out mechanism

--- lib/widgets/menu_list_tile.dart
+++ lib/widgets/menu_list_tile.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:habs/Authentication/authentication_service.dart';
 import 'package:habs/pages/appointment.dart';
+import 'package:habs/pages/home.dart';
 import 'package:habs/pages/hospital_services.dart';
 import 'package:habs/pages/doctors.dart';
+import 'package:habs/pages/tools/welcome_page.dart';
+import 'package:provider/provider.dart';
 
 class MenuListTileWidget extends StatefulWidget {
   @override
@@ -17,48 +21,59 @@ class _MenuListTileWidgetState extends State<MenuListTileWidget> {
         ListTile(
           leading: Icon(Icons.local_hospital),
           title: Text('Services Offered'),
-          onTap: (){
+          onTap: () {
             Navigator.pop(context);
             Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => HospitalServices(),
-                )
-            );
+                ));
           },
         ),
         ListTile(
           leading: Icon(Icons.tag_faces),
           title: Text('Doctors'),
-          onTap: (){
+          onTap: () {
             Navigator.pop(context);
             Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => Doctors(),
-                )
-            );
+                ));
           },
         ),
         ListTile(
-          leading: Icon(Icons.assignment),
+          leading: Icon(Icons.book_online),
           title: Text('Appointments'),
-          onTap: (){
+          onTap: () {
             Navigator.pop(context);
             Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => Appointments(),
-                )
-            );
+                ));
           },
         ),
+
         Divider(),
         ListTile(
           leading: Icon(Icons.settings),
           title: Text('Settings'),
-          onTap: (){
+          onTap: () {
             Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.lock_open),
+          title: Text('Log Out'),
+          onTap: () {
+            context.read<AuthenticationService>().signOut();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Welcome(),
+              ),
+            );
           },
         ),
       ],

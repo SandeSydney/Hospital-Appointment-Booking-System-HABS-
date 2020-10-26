5d8e54c7aef41f3ccceb5a51e96e70e237957613
SandeSydney <sandesydney01@gmail.com>
2020-10-26 05:10:16 +0300

    Added Auth Wrapper and Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:habs/Authentication/authentication_service.dart';
+import 'package:habs/pages/home.dart';
 import 'package:habs/pages/tools/login_page.dart';
 import 'package:habs/pages/tools/welcome_page.dart';
+import 'package:provider/provider.dart';
 
 // main method of the application
 Future<void> main() async {
+  // initialize the firebase app
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
@@ -14,13 +19,39 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'habs',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    return MultiProvider(
+      // provide the auth class and the user stream
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+            create: (context) =>
+                context.read<AuthenticationService>().authStateChanges),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'habs',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: AuthenticationWrapper(),
       ),
-      home: Welcome(),
     );
+  }
+}
+
+// to return login page or home page whether you are authenticated or not
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //listen for the user from the stream
+    final firebaseUser = context.watch<User>();
+
+    // respond to the results of either a user or null
+    if (firebaseUser != null) {
+      return Home();
+    }
+    return Welcome();
   }
 }
